7b495eefbd326e9af5e1dca28a5e03345ded4edd
Ravi <ravic1919@gmail.com>
2020-02-18 09:21:18 +0000

    Update kata.js

--- kata.js
+++ kata.js
@@ -1,11 +1,7 @@
 const numbers = [33, 45, 23, 5, 99, 44, 88, 53, 101, 85];
 
 function ghostPassword(numbers) {
-  return numbers
-    .toString()
-    .split(",")
-    .reverse()
-    .join(" ");
+  return numbers;
 }
 
 // There is a ghost trapped in the toilets at the top of the Custard Factory and he needs the right numbers to escape the building.
