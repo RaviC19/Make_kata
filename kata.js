2c1f45f81e3c1fde0b7befc3df8ff36f95b5dc5b
RaviC19 <ravic1919@gmail.com>
2020-02-18 03:36:19 +0000

    Working kata and tests v2

--- kata.js
+++ kata.js
@@ -1,4 +1,5 @@
 const numbers = [33, 45, 23, 5, 99, 44, 88, 53, 101, 85];
+
 function ghostPassword(numbers) {
   return numbers
     .toString()
@@ -6,6 +7,8 @@ function ghostPassword(numbers) {
     .reverse()
     .join(" ");
 }
+
+//Final output should be "85 101 53 88 44 99 5 23 45 33"
 
 module.exports = {
   ghostPassword
