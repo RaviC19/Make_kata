5512e19b2120050856c061a73e210a9d72ab0943
RaviC19 <ravic1919@gmail.com>
2020-02-18 09:17:15 +0000

    No Answers

--- kata.js
+++ kata.js
@@ -8,6 +8,10 @@ function ghostPassword(numbers) {
     .join(" ");
 }
 
+// There is a ghost trapped in the toilets at the top of the Custard Factory and he needs the right numbers to escape the building.
+// They have been given an array of numbers, which they need to alter to strings and reverse the number order. Commas also need to be removed.
+// E.g [33, 45, 23, 5] would become "5 23 45 33"
+
 //Final output should be "85 101 53 88 44 99 5 23 45 33"
 
 module.exports = {
